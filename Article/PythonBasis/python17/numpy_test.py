ba59fa8cd0df2e5e0d39330269ed851f0bf619c0
amanda <zhujianwang001@gmail.com>
2021-09-24 17:11:55 +0800

    添加python高级函数使用说明

--- Article/PythonBasis/python17/numpy_test.py
+++ Article/PythonBasis/python17/numpy_test.py
@@ -8,7 +8,10 @@ a = np.array([[0, 1, 2, 3, 4, 5],
 
 print("\n a[2::2, ::2]=\n", a[2::2, ::2])
 
+print("\n a[2::2, ::-1]=\n", a[2::2, ::-1]) # ::-1表示从最后一列开始往前数，
+
 # A 3 dimensional array.
+# ellipsis index operator
 b = np.array([
     [[1, 2, 3], [4, 5, 6]],
     [[7, 8, 9], [10, 11, 12]],
@@ -25,4 +28,29 @@ a = np.array([[1, 2],
 print(a[[0, 1, 2], [0, 0, 1]]) # Equivalent to a[0, 0] a[1, 0] a[2, 1]
 print(*zip([0, 1, 2], [0, 0, 1]))
 
+a = np.array([[0, 1, 2],
+              [3, 4, 5],
+              [6, 7, 8],
+              [9, 10, 11]])
+print(a.shape)
+print(a[1:2, 1:3])
+print(a[1:2, [1, 2]])
 
+a = np.array([10, 40, 80, 50, 100])
+print(a[a>50])
+# [80 100]
+
+a = np.array([10, 40, 80, 50, 100])
+print(a[a%40==0]**2)
+# [1600 6400]
+
+
+b = np.array([[5, 5],
+              [4, 5],
+              [16, 4]])
+sumrow = b.sum(1) # 按行求和，求取的结果的行数与b的行数一致，
+print(sumrow)
+print(sumrow%10==0)
+print(b[sumrow%10==0])
+
+print(*[f'{x}_curve.png' for x in ('F1', 'PR', 'P', 'R')])
