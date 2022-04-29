d93832c313d98d7a17eddf4d59ed66c4e9a9810c
zhujianwang <zhujianwang001@outlook.com>
2022-04-29 15:56:03 +0800

    yaml实现自定义类的序列化与反序列化教程

--- ThirdLib/yaml/README.md
+++ ThirdLib/yaml/README.md
@@ -20,8 +20,31 @@ if __name__ == '__main__':
     print(dump({'name': 'Silenthand Olleander', 'race': 'Human', 'traits': ['ONE_HAND', 'ONE_EYE']}, Dumper=Dumper, default_flow_style=True))
 ```
 
-## add_constructor函数
+## python自定义类继承yaml.YAMLObject实现数据的序列化与反序列化
+* yaml.YAMLObject用元类来注册一个构造器（也就是代码里的 init() 方法），让你把yaml节点转为Python对象实例，用表示器（也就是代码里的 repr() 函数）来让你把Python对象转为yaml节点
+```python
+import yaml
+class Person(yaml.YAMLObject):
+    yaml_tag = '!person'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return '%s(name=%s, age=%d)' % (self.__class__.__name__, self.name, self.age)
+
+if __name__ == '__main__':
+    james = Person('James', 20)
+    print(yaml.dump(james, default_flow_style=True))  # Python对象实例转为yaml
+    lily = yaml.load('!person {name: Lily, age: 19}')
+    print(lily)  # yaml转为Python对象实例
+```
+
+
+## yaml.add_constructor 和 yaml.add_representer函数
 * 
 
 ## 参考链接
 * 1 [PyYAML官方文档教程](https://pyyaml.org/wiki/PyYAMLDocumentation)
+* 2 [yaml实现自定义类的序列化与反序列化教程](https://www.cnblogs.com/klb561/p/9326677.html)
--- ThirdLib/yaml/yaml_test.py
+++ ThirdLib/yaml/yaml_test.py
@@ -1,20 +1,36 @@
+import yaml
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
+class Person(yaml.YAMLObject):
+    yaml_tag = '!person'
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return '%s(name=%s, age=%d)' % (self.__class__.__name__, self.name, self.age)
 
 if __name__ == '__main__':
-    stream = file('document.yaml', 'w')
 
-    document = """
-      a: 1
-      b:
-        c: 3
-        d: 4
-    """
-    print(dump(load(document, Loader=Loader), Dumper=Dumper, default_flow_style=True))
-    print(dump({'name': 'Silenthand Olleander', 'race': 'Human', 'traits': ['ONE_HAND', 'ONE_EYE']}, Dumper=Dumper, default_flow_style=True))
+    # document = """
+    #   a: 1
+    #   b:
+    #     c: 3
+    #     d: 4
+    # """
+    # print(dump(load(document, Loader=Loader), Dumper=Dumper, default_flow_style=True))
+    # print(dump({'name': 'Silenthand Olleander', 'race': 'Human', 'traits': ['ONE_HAND', 'ONE_EYE']}, Dumper=Dumper, default_flow_style=True))
+
+    james = Person('James', 20)
+
+    print(yaml.dump(james, default_flow_style=True))  # Python对象实例转为yaml
+
+    lily = yaml.load('!person {name: Lily, age: 19}')
+
+    print(lily)  # yaml转为Python对象实例
 
 
