8f592ff430b9a27ba040dd40b18142c0a4fa9397
E00130 <barbara.zhu@icleague.com>
2022-07-19 09:25:19 +0800

    typing模块的类型别名教程

--- ThirdLib/typing/src/type_alias.py
+++ ThirdLib/typing/src/type_alias.py
@@ -7,3 +7,11 @@ def scale(scalar: float, vector: Vector) -> Vector:
 # typechecks; a list of floats qualifies as a Vector.
 new_vector = scale(2.0, [1.0, -4.2, 5.4])
 print(new_vector)
+
+import time
+import sys
+
+for i in range(5):
+    print(i),
+    sys.stdout.flush()
+    time.sleep(1)
