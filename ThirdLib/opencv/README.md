f2c2bb22c98ffaa776d0ecb3018676d1d52bc4e7
E00130 <barbara.zhu@icleague.com>
2022-07-28 14:20:49 +0800

    添加手动实现opencv的梯度运算操作

--- ThirdLib/opencv/README.md
+++ ThirdLib/opencv/README.md
@@ -4,12 +4,23 @@
 ## [`cv2.threshold`](https://www.bilibili.com/video/BV1PV411774y?p=9&vd_source=df8b73f15b5a0311fd8a1646ccd07daf)函数对图像设置阈值参数进行过滤操作
 
 ## [`cv2.erode`图像腐蚀操作](https://www.bilibili.com/video/BV1PV411774y?p=12&vd_source=df8b73f15b5a0311fd8a1646ccd07daf)
-* `kernel_size`大小的卷积核对二值图像进行滑动窗口卷积操作，做完卷积后的矩阵中只要有一个元素值为0，则这一块被卷积的区域则置0处理。
+* `kernel_size`大小元素值为1的卷积核对二值图像进行滑动窗口卷积操作，做完卷积后的矩阵中只要有一个元素值为0，则这一块被卷积的区域则置0处理。
 
 ## [`cv2.dilate`图像膨胀操作](https://www.bilibili.com/video/BV1PV411774y?p=12&vd_source=df8b73f15b5a0311fd8a1646ccd07daf)
-* `kernel_size`大小的卷积核对二值图像进行滑动窗口卷积操作，做完卷积后的矩阵中只要有一个元素值为0，则这一块被卷积的区域则置1处理。
+* `kernel_size`大小元素值为1的卷积核对二值图像进行滑动窗口卷积操作，做完卷积后的矩阵中只要有一个元素值为0，则这一块被卷积的区域则置1处理。
+
+## [`形态学操作之开运算与闭运算`](https://www.bilibili.com/video/BV1PV411774y?p=14&vd_source=df8b73f15b5a0311fd8a1646ccd07daf)
+* 开运算是指对二值图先进行腐蚀再进行膨胀操作，闭运算则相反。
+```python
+kernel = np.ones((5,5), np.unit8)
+opening = cv2.morphorlogyEx(img, cv2.MORPO_OPEN, kernel)
+closing = cv2.morphorlogyEx(img, cv2.MORPO_ClOSE, kernel)
+img_statck = np.vstatck((img, opening, closing))
+
+```
 
 ## 
+
 
 ## 参考链接
 * 1 [`opencv`视频教程](https://www.bilibili.com/video/BV1PV411774y?p=9&vd_source=df8b73f15b5a0311fd8a1646ccd07daf)
---
+++ ThirdLib/opencv/src/bitwiseTest.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+ 
+# 在画布中心画了一个300*300像素的矩形，用白色填充
+# 注意：这是一个二维矩阵，反映在图片上是单通道的
+rectangle = np.zeros((300, 300), dtype="uint8")
+# 第二个参数为矩形左上角的点的坐标，第三个参数为矩形右下角的点的坐标
+cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+# cv2.imshow("Rectangle", rectangle)
+ 
+# 在画布中心画了一个半径150像素的圆圈，用白色填充
+# 注意：这是一个二维矩阵，反映在图片上是单通道的
+circle = np.zeros((300, 300), dtype="uint8")
+cv2.circle(circle, (150, 150), 150, 255, -1)
+# cv2.imshow("Circle", circle)
+ 
+# 按位与操作AND
+bitwiseAnd = cv2.bitwise_and(rectangle, circle)
+# cv2.imshow("AND", bitwiseAnd)
+
+# 按位或操作OR
+bitwiseOr = cv2.bitwise_or(rectangle, circle)
+# cv2.imshow("OR", bitwiseOr)
+
+ 
+# 按位异或XOR
+bitwiseXor = cv2.bitwise_xor(rectangle, circle)
+# cv2.imshow("XOR", bitwiseXor)
+ 
+# 按位非NOT
+bitwiseNot = cv2.bitwise_not(circle)
+# cv2.imshow("NOT", bitwiseNot)
+ 
+# cv2.waitKey(0)  # 等待用户输入，按任意键即可
+img_joint = np.hstack((bitwiseAnd, bitwiseOr, bitwiseXor, bitwiseXor, bitwiseNot))
+print(1)
+
+# 梯度运算
+kernel = np.ones((7, 7), np.uint8)
+dilate = cv2.dilate(circle,kernel=kernel,iterations=5)
+erosion = cv2.erode(circle,kernel=kernel,iterations=5)
+dilate_erosion_xor = cv2.bitwise_xor(dilate, erosion)
+res1 = np.hstack((circle, dilate))
+res2 = np.hstack((erosion, dilate_erosion_xor))
+gradient_res1 = np.vstack((res1, res2))
+
+# 使用形态学函数cv2.morphologyEx直接进行梯度运算操作
+gradient = cv2.morphologyEx(circle, cv2.MORPH_GRADIENT, kernel=kernel)
+gradient_vs = np.hstack((dilate_erosion_xor, gradient))
+print(2)
