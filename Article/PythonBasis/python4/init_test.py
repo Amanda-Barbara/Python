e1c37d3aa9ebf412710a1933cf01d45c4a71f17c
E00130 <barbara.zhu@icleague.com>
2022-11-02 15:10:53 +0800

    添加geta

--- Article/PythonBasis/python4/init_test.py
+++ Article/PythonBasis/python4/init_test.py
@@ -12,7 +12,7 @@ class User(object):
     def get_addr(self, addr:str):
         if addr:
             self.addr = addr
-        return self.addr
+        return self.addr*2
 
 if __name__ == '__main__':
     print(User.name1)
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     user.id = 123
     print(user.category)
     print(user.id)
-    print(getattr(User,'get_addr',2))
+    print(getattr(User,'get_addr1',"mm")) #加上参数"mm",如果没有找到User的属性值get_addr1则会默认输出"mm",如果不加参数，找不到User的属性值程序则会报错
+    #print(getattr(User,'get_addr1')) 
     get_addr2 = getattr(user, 'get_addr')
     print(get_addr2('nn'))
